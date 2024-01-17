7c0a819f6a4772d5e9d04a6f5cf26958e759c3f3
pranayjoshi <pranayjoshi4466@gmail.com>
2024-01-17 01:18:32 -0600

    Added

--- lib/widgets/feature_suite.dart
+++ lib/widgets/feature_suite.dart
@@ -3,25 +3,29 @@ import 'package:flutter/material.dart';
 class FeatureSuite extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(10),
-      child: GridView.count(
-        crossAxisCount: 2,
-        childAspectRatio: 1.0,
-        mainAxisSpacing: 10.0,
-        crossAxisSpacing: 10.0,
-        children: <Widget>[
-          _createGridItem('Box 1', Colors.lightBlue[100]!),
-          _createGridItem('Box 2', Colors.lightGreen[100]!),
-          _createGridItem('Box 3', Colors.pink[100]!),
-          _createGridItem('Box 4', Colors.yellow[100]!),
-        ],
+    return Expanded(
+      child: Container(
+        height: MediaQuery.of(context).size.height * 0.4,
+        padding: EdgeInsets.all(10),
+        child: GridView.count(
+          crossAxisCount: 2,
+          childAspectRatio: 1.3,
+          mainAxisSpacing: 10.0,
+          crossAxisSpacing: 10.0,
+          children: <Widget>[
+            _createGridItem('Box 1', Colors.lightBlue[400]!, context),
+            _createGridItem('Box 2', Colors.lightGreen[400]!, context),
+            _createGridItem('Box 3', Colors.pink[400]!, context),
+            _createGridItem('Box 4', Colors.yellow[400]!, context),
+          ],
+        ),
       ),
     );
   }
 
-  Widget _createGridItem(String title, Color color) {
-    return Container(
+Widget _createGridItem(String title, Color color, BuildContext context) {
+  return Container(
+    height: MediaQuery.of(context).size.height * 0.01,
       decoration: BoxDecoration(
         color: color,
         borderRadius: BorderRadius.circular(10),
@@ -33,5 +37,5 @@ class FeatureSuite extends StatelessWidget {
         ),
       ),
     );
-  }
+}
 }
