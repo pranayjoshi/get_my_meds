1792c56cea9a46886e44e34ecc9394cc90be0a23
pranayjoshi <pranayjoshi4466@gmail.com>
2024-01-16 22:36:31 -0600

    Added

---
+++ lib/layouts/home_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Text('Your main content goes here'),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.home),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
+            ),
+            SizedBox(
+              width: 48.0,
+            ),
+            IconButton(
+              icon: Icon(Icons.person),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.settings),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Container(
+        width: 80.0,
+        height: 80.0,
+        margin: EdgeInsets.only(bottom: 10.0),
+        child: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(
+            Icons.camera_alt,
+            size: 40.0,
+          ),
+          elevation: 1.0,
+          shape: CircleBorder(),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get_my_meds/layouts/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,64 +9,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
-    );
+      title: 'Get My Meds',
+      theme: ThemeData(
+        primaryColor: Colors.green,
+        colorScheme: ColorScheme.light(
+          primary: Colors.green,
+          secondary: Colors.green,
+        ),
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: AppBarTheme(
+          toolbarTextStyle: TextTheme(
+            displaySmall: TextStyle(
+              color: Colors.white,
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ).bodySmall
+        ),),
+        home: HomePage()
+      );
+      
   }
 }
 
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-      ),
-      body: Center(
-        child: Text('Your main content goes here'),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        shape: CircularNotchedRectangle(),
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            IconButton(
-              icon: Icon(Icons.home),
-              onPressed: () {},
-            ),
-            IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () {},
-            ),
-            SizedBox(
-              width: 48.0,
-            ),
-            IconButton(
-              icon: Icon(Icons.person),
-              onPressed: () {},
-            ),
-            IconButton(
-              icon: Icon(Icons.settings),
-              onPressed: () {},
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: Container(
-        width: 80.0,
-        height: 80.0,
-        margin: EdgeInsets.only(bottom: 10.0),
-        child: FloatingActionButton(
-          onPressed: () {},
-          child: Icon(
-            Icons.camera_alt,
-            size: 40.0,
-          ),
-          elevation: 1.0,
-          shape: CircleBorder(),
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-    );
-  }
-}
