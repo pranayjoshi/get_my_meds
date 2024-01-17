30ce206c3a04e7f3168d92c4e1fea6d094f0837d
pranayjoshi <pranayjoshi4466@gmail.com>
2024-01-17 00:23:38 -0600

    Added

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -8,7 +8,7 @@ class WeeklyProgressChart extends StatelessWidget {
       alignment: Alignment.topCenter,
       height: MediaQuery.of(context).size.height * 0.4,
       child: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(16.0),
         child: Container(
           decoration: BoxDecoration(
             color: Colors.white,
@@ -30,34 +30,66 @@ class WeeklyProgressChart extends StatelessWidget {
                   'Weekly Progress',
                   style: TextStyle(
                     fontSize: 14,
-                    color: ColorScheme.light().primary,
+                    color: Theme.of(context).colorScheme.secondary,
                     fontWeight: FontWeight.bold,
-                  )
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
                 ),
                 Expanded(
                   child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
                     child: LineChart(
                       LineChartData(
                         gridData: FlGridData(show: false),
-                        titlesData: FlTitlesData(show: false),
+                        titlesData: FlTitlesData(
+                          show: true,
+                          bottomTitles: SideTitles(
+                            showTitles: true,
+                            getTitles: (value) {
+                              switch (value.toInt()) {
+                                case 0:
+                                  return 'Mon';
+                                case 1:
+                                  return 'Tue';
+                                case 2:
+                                  return 'Wed';
+                                case 3:
+                                  return 'Thu';
+                                case 4:
+                                  return 'Fri';
+                                case 5:
+                                  return 'Sat';
+                                case 6:
+                                  return 'Sun';
+                                default:
+                                  return '';
+                              }
+                            },
+                          ),
+                          leftTitles: SideTitles(
+                            showTitles: true,
+                            getTitles: (value) => value.toInt().toString(),
+                          ),
+                        ),
                         borderData: FlBorderData(show: false),
                         lineBarsData: [
                           LineChartBarData(
                             spots: [
                               FlSpot(0, 1),
-                              FlSpot(1, 2),
-                              FlSpot(2, 3),
-                              FlSpot(3, 4),
-                              FlSpot(4, 5),
-                              FlSpot(5, 6),
-                              FlSpot(6, 7),
+                              FlSpot(1, 3),
+                              FlSpot(2, 2),
+                              FlSpot(3, 5),
+                              FlSpot(4, 3),
+                              FlSpot(5, 4),
+                              FlSpot(6, 6),
                             ],
-                            isCurved: true,
-                            colors: [Colors.blue],
+                            isCurved: false,
+                            colors: [Theme.of(context).colorScheme.secondary],
                             barWidth: 2,
                             isStrokeCapRound: true,
-                            dotData: FlDotData(show: false),
+                            dotData: FlDotData(show: true),
                             belowBarData: BarAreaData(show: false),
                           ),
                         ],
