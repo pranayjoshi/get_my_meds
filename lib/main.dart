aadf0def84311e5d8ef51bfa0db2e676aeeb276a
pranayjoshi <pranayjoshi4466@gmail.com>
2024-01-24 20:41:12 -0600

    added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:get_my_meds/layouts/home_page.dart';
+import 'package:permission_handler/permission_handler.dart';
 List<CameraDescription> cameras = [];
+
 void main() async {
-  
   WidgetsFlutterBinding.ensureInitialized();
+
+  var status = await Permission.camera.status;
+  if (!status.isGranted) {
+    status = await Permission.camera.request();
+    if (!status.isGranted) {
+      // Handle the case where the user did not grant the permission
+      return;
+    }
+  }
+
   cameras = await availableCameras();
   runApp(MyApp());
 }
