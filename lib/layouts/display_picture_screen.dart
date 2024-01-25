eeba7c7b2f458ce03a7ba407bd2208548b65c6c1
pranayjoshi <pranayjoshi4466@gmail.com>
2024-01-24 20:08:31 -0600

    added

--- lib/layouts/display_picture_screen.dart
+++ lib/layouts/display_picture_screen.dart
@@ -11,6 +11,30 @@ class DisplayPictureScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text('Display the Picture')),
       body: Image.file(File(imagePath)),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.scanner), // Replace with your scan logo
+        onPressed: () async {
+          showDialog(
+            context: context,
+            barrierDismissible: false,
+            builder: (BuildContext context) {
+              return Dialog(
+                child: new Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    new CircularProgressIndicator(),
+                    new Text("Loading"),
+                  ],
+                ),
+              );
+            },
+          );
+          await Future.delayed(Duration(seconds: 2));
+          Navigator.pop(context); // pop the dialog
+          Navigator.pop(context); // pop the screen
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
--- lib/layouts/take_picture_screen.dart
+++ lib/layouts/take_picture_screen.dart
@@ -1,5 +1,6 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:get_my_meds/layouts/display_picture_screen.dart';
 
 class TakePictureScreen extends StatefulWidget {
   final CameraDescription camera;
