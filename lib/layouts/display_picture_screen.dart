a55942094f82b0f7cd6daaad21270acdcd3f4a87
pranayjoshi <pranayjoshi4466@gmail.com>
2024-01-24 20:07:22 -0600

    added

---
+++ lib/layouts/display_picture_screen.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+
+  const DisplayPictureScreen({Key? key, required this.imagePath}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Display the Picture')),
+      body: Image.file(File(imagePath)),
+    );
+  }
+}
